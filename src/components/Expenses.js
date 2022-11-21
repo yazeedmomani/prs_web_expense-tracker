65e889d886bf74d2497dd0004cd72fe12e606df1
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-21 23:22:58 +0300

    create expenses.js

---
+++ src/components/Expenses.js
@@ -0,0 +1,31 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        date={props.items[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+      />
+      <ExpenseItem
+        date={props.items[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+      />
+      <ExpenseItem
+        date={props.items[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+      />
+      <ExpenseItem
+        date={props.items[3].date}
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
