95d6a455e9286abeda84d2f5f7c4b28474610b48
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2022-11-22 22:18:22 +0300

    create handlers

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,18 +1,45 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 function ExpenseForm() {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: title,
+      amount: amount,
+      date: new Date(date),
+    };
+
+    console.log(expenseData);
+
+    setTitle("");
+    setAmount("");
+    setDate("");
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
           <input
             type="text"
             onChange={titleChangeHandler}
+            value={title}
           />
         </div>
         <div className="new-expense__control">
@@ -21,6 +48,8 @@ function ExpenseForm() {
             type="number"
             min="0.01"
             step="0.01"
+            onChange={amountChangeHandler}
+            value={amount}
           />
         </div>
         <div className="new-expense__control">
@@ -29,6 +58,8 @@ function ExpenseForm() {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            onChange={dateChangeHandler}
+            value={date}
           />
         </div>
       </div>
